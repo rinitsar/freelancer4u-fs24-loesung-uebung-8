a91cb827d722d45af8763c855d6022b2c067cbfe
rinitsar <145468257+rinitsar@users.noreply.github.com>
2024-04-24 12:11:52 +0200

    application.properties und auth.config.js geändert

--- freelancer4u-fs24-loesung-uebung-8/freelancer4u-frontend/src/auth.config.js
+++ freelancer4u-fs24-loesung-uebung-8/freelancer4u-frontend/src/auth.config.js
@@ -1,6 +1,6 @@
 const config = {
-    auth0_domain: "...",
-    auth0_client_id: "..."
+    auth0_domain: "dev-yxeh70xuu7rnx2cu.us.auth0.com",
+    auth0_client_id: "vscpGCrzR6UgPdiRhO7Ff0R7Zq9eNd5V"
 };
   
 export default config;
