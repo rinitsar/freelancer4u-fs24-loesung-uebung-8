9031fca2894f9090b31ce2ad11c8ee7a9e10421f
rinitsar <145468257+rinitsar@users.noreply.github.com>
2024-04-24 14:55:14 +0200

    JobRepository angepasst

--- freelancer4u-fs24-loesung-uebung-8/src/main/java/ch/zhaw/freelancer4u/repository/JobRepository.java
+++ freelancer4u-fs24-loesung-uebung-8/src/main/java/ch/zhaw/freelancer4u/repository/JobRepository.java
@@ -1,15 +1,14 @@
 package ch.zhaw.freelancer4u.repository;
 
-import java.util.List;
-import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ch.zhaw.freelancer4u.model.Job;
-import ch.zhaw.freelancer4u.model.JobStateAggregation;
 import ch.zhaw.freelancer4u.model.JobType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
 public interface JobRepository extends MongoRepository<Job,String>{
-    List<Job> findByEarningsGreaterThan(Double earnings);
-    List<Job> findByJobType(JobType jobType);
-    @Aggregation("{$group:{_id:'$jobState',jobIds:{$push:'$_id',},count:{$count:{}}}}")
-    List<JobStateAggregation> getJobStateAggregation();
+    Page<Job> findByEarningsGreaterThan(Double earnings, Pageable pageable);
+Page<Job> findByJobType(JobType jobType, Pageable pageable);
+ Page<Job> findByJobTypeAndEarningsGreaterThan(JobType jobType, Double earnings,
+Pageable pageable);
 }
