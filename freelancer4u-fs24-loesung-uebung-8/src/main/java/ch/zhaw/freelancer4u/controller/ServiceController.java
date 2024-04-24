13c96dccf70cbf721645dd239f7d460c0a09c3a8
rinitsar <145468257+rinitsar@users.noreply.github.com>
2024-04-24 15:35:45 +0200

    PutMapping ergänzt

--- freelancer4u-fs24-loesung-uebung-8/src/main/java/ch/zhaw/freelancer4u/controller/ServiceController.java
+++ freelancer4u-fs24-loesung-uebung-8/src/main/java/ch/zhaw/freelancer4u/controller/ServiceController.java
@@ -3,7 +3,7 @@ package ch.zhaw.freelancer4u.controller;
 import java.util.Optional;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,4 +45,29 @@ public class ServiceController {
        //...
     return null;
        }
+
+    @PutMapping("/me/assignjob")
+    public ResponseEntity<Job> assignToMe(@RequestParam String jobId,
+     @AuthenticationPrincipal Jwt jwt) {
+    String userEmail = jwt.getClaimAsString("email");
+    JobService jobService = new JobService(); // Initialize jobService
+    Optional<Job> job = jobService.assignJob(jobId, userEmail);
+    if (job.isPresent()) {
+    return new ResponseEntity<>(job.get(), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+
+@PutMapping("/me/completejob")
+public ResponseEntity<Job> completeJob(@RequestParam String jobId, @AuthenticationPrincipal Jwt jwt) {
+    String userEmail = jwt.getClaimAsString("email");
+    JobService jobService = new JobService(); // Initialize jobService
+    Optional<Job> job = jobService.completeJob(jobId, userEmail);
+    if (job.isPresent()) {
+        return new ResponseEntity<>(job.get(), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+}
+
 }
